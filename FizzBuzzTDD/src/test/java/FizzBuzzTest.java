5cee1cb11f7a354c393f4cef722a9aff71fd3257
hantczak <hantczak8@gmail.com>
2021-03-25 14:28:51 +0100

    FizzBuzzTDD test class refactoring

--- FizzBuzzTDD/src/test/java/FizzBuzzTest.java
+++ FizzBuzzTDD/src/test/java/FizzBuzzTest.java
@@ -1,37 +1,37 @@
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class FizzBuzzTest {
+class FizzBuzzTest {
     private FizzBuzz fizzBuzz;
 
-    @Before
+    @BeforeEach
     public void initialize() {
         fizzBuzz = new FizzBuzz();
     }
 
     @Test
-    public void shouldProcessSingleNumber() {
+    void shouldProcessSingleNumber() {
         assertEquals("1", fizzBuzz.process(1));
         assertEquals("2", fizzBuzz.process(2));
     }
 
     @Test
-    public void shouldReturnFizzWithNumberDivisibleByThree() {
+    void shouldReturnFizzWithNumberDivisibleByThree() {
         assertEquals("Fizz", fizzBuzz.process(3));
-        assertEquals("Fizz",fizzBuzz.process(33));
+        assertEquals("Fizz", fizzBuzz.process(33));
     }
 
     @Test
-    public void shouldReturnBuzzWithNumberDivisibleByFive(){
-        assertEquals("Buzz",fizzBuzz.process(5));
-        assertEquals("Buzz",fizzBuzz.process(65));
+    void shouldReturnBuzzWithNumberDivisibleByFive() {
+        assertEquals("Buzz", fizzBuzz.process(5));
+        assertEquals("Buzz", fizzBuzz.process(65));
     }
 
     @Test
-    public void shouldReturnFizzBuzzWithNumberDivisibleByBothFiveAndThree(){
-        assertEquals("FizzBuzz",fizzBuzz.process(15));
-        assertEquals("FizzBuzz",fizzBuzz.process(60));
+    void shouldReturnFizzBuzzWithNumberDivisibleByBothFiveAndThree() {
+        assertEquals("FizzBuzz", fizzBuzz.process(15));
+        assertEquals("FizzBuzz", fizzBuzz.process(60));
     }
 }
