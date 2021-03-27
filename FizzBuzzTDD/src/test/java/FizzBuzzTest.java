21e91dac9561d835b646ce99d2b34121efcb897f
hantczak <hantczak8@gmail.com>
2021-03-27 15:38:15 +0100

    AssertAll used instead of series of Assertions

--- FizzBuzzTDD/src/test/java/FizzBuzzTest.java
+++ FizzBuzzTDD/src/test/java/FizzBuzzTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 class FizzBuzzTest {
     private FizzBuzz fizzBuzz;
@@ -13,25 +14,33 @@ class FizzBuzzTest {
 
     @Test
     void shouldProcessSingleNumber() {
-        assertEquals("1", fizzBuzz.process(1));
-        assertEquals("2", fizzBuzz.process(2));
+        assertAll(
+                () -> assertEquals("1", fizzBuzz.process(1)),
+                () -> assertEquals("2", fizzBuzz.process(2))
+        );
     }
 
     @Test
     void shouldReturnFizzWithNumberDivisibleByThree() {
-        assertEquals("Fizz", fizzBuzz.process(3));
-        assertEquals("Fizz", fizzBuzz.process(33));
+        assertAll(
+                () -> assertEquals("Fizz", fizzBuzz.process(3)),
+                () -> assertEquals("Fizz", fizzBuzz.process(33))
+        );
     }
 
     @Test
     void shouldReturnBuzzWithNumberDivisibleByFive() {
-        assertEquals("Buzz", fizzBuzz.process(5));
-        assertEquals("Buzz", fizzBuzz.process(65));
+        assertAll(
+                () -> assertEquals("Buzz", fizzBuzz.process(5)),
+                () -> assertEquals("Buzz", fizzBuzz.process(65))
+        );
     }
 
     @Test
     void shouldReturnFizzBuzzWithNumberDivisibleByBothFiveAndThree() {
-        assertEquals("FizzBuzz", fizzBuzz.process(15));
-        assertEquals("FizzBuzz", fizzBuzz.process(60));
+        assertAll(
+                () -> assertEquals("FizzBuzz", fizzBuzz.process(15)),
+                () -> assertEquals("FizzBuzz", fizzBuzz.process(60))
+        );
     }
 }
